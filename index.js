0719c82124e19c949b06df0c65c837db4613cb0f
allinthemind66 <r.nilon92@gmail.com>
2018-03-12 22:06:53 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+const dog = {name: 'fido', age: 2}
+for (const key in dog){
+  console.log(key)
+}
+
+function forEach(iterable, callback){
+  if(Array.isArray(iterable)){
+    for(let i = 0; i < iterable.length; i++){
+      const element = iterable[i]
+      callback(element, i, iterable)
+    }
+  }else if (typeof iterable === 'object'){
+    for (const key in iterable){
+      const value = iterable[key]
+      callback(value, key, iterable)
+    }
+  }
+}
